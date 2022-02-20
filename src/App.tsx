f44904a43e033691be6d822d1265daf7fd527cee
Tom Steele <tsteele@madisoncollege.edu>
2022-02-19 21:18:42 -0600

    Some navigation and some theme...
    Found a nice tutorial in an old MS bootcamp from that guy I follow on Twitter...

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import { Home } from './Home';
+import { loadTheme } from '@fluentui/react';
 
 const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
 const stackTokens: IStackTokens = { childrenGap: 15 };
@@ -12,6 +13,33 @@ const SetupGame = () => <h2>Setup Game</h2>;
 const PlayGame = () => <h2>Play Game</h2>;
 
 export const App: React.FunctionComponent = () => {
+
+  loadTheme({
+    palette: {
+      themePrimary: '#8c1833',
+      themeLighterAlt: '#faf2f4',
+      themeLighter: '#edcdd5',
+      themeLight: '#dda6b2',
+      themeTertiary: '#ba5d73',
+      themeSecondary: '#9a2a44',
+      themeDarkAlt: '#7e152e',
+      themeDark: '#6b1227',
+      themeDarker: '#4f0d1d',
+      neutralLighterAlt: '#eeebe9',
+      neutralLighter: '#eae7e6',
+      neutralLight: '#e1dedc',
+      neutralQuaternaryAlt: '#d1cecd',
+      neutralQuaternary: '#c8c5c4',
+      neutralTertiaryAlt: '#c0bdbc',
+      neutralTertiary: '#a29ecf',
+      neutralSecondary: '#5b549f',
+      neutralPrimaryAlt: '#2a2472',
+      neutralPrimary: '#1b155e',
+      neutralDark: '#141048',
+      black: '#0f0c35',
+      white: '#f5f1f0',
+    }});
+
   return (
     <Stack horizontalAlign="center" verticalAlign="start" verticalFill tokens={stackTokens}>
       <Routes>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,25 @@
 import { useNavigate } from 'react-router-dom';
+import { DefaultButton, PrimaryButton, CompoundButton } from '@fluentui/react/lib/Button';
+import { Text } from '@fluentui/react/lib/Text';
+import { Stack } from '@fluentui/react/lib/Stack';
 
 export const Home = () => {
     const nav = useNavigate();
 
     return(
-        <>
-        <h2>
-            Home
-        </h2>
-        <button 
-            onClick={() => nav('/setup-game')}>
+        <Stack>
+            <Text
+                variant='xxLarge'
+            >
+                Home
+            </Text>
+            <CompoundButton
+                primary
+                secondaryText='Grab your game & play' 
+                onClick={() => nav('/setup-game')}
+            >
                 Play
-            </button>
-        </>
+            </CompoundButton>
+        </Stack>
     );
 }
