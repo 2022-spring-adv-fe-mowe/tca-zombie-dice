9ac03b11e175d80179222fee93e0dcdcca98fd26
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-03-05 17:06:23 -0600

    Some initial player buttons on a Panel...
    Hmm, maybe I like it ? ? ?

--- src/PlayGame.tsx
+++ src/PlayGame.tsx
@@ -3,6 +3,8 @@ import { Text } from '@fluentui/react/lib/Text';
 import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
 import { useNavigate } from 'react-router-dom';
 import { CurrentGame } from './App';
+import { useState, useEffect } from 'react';
+import { Panel, PanelType } from '@fluentui/react/lib/Panel';
 
 interface PlayGameProps {
     currentGame: CurrentGame
@@ -12,8 +14,42 @@ export const PlayGame: React.FC<PlayGameProps> = ({currentGame}) => {
 
     const nav = useNavigate();
 
+    const [playerOrderChosenCount, setPlayerOrderChosenCount] = useState(0);
+
+    // Use effect to pop panel for choosing player turn ? ? ?
+    useEffect(
+        () => {
+            console.log('here');
+        }
+        , []
+    );
+
     return (
         <Stack style={{padding: 30}}>
+            <Panel
+                type={PanelType.smallFixedNear}
+                hasCloseButton={false}
+                isOpen={true}
+                headerText="Player 1"
+            >
+                <Stack
+                    tokens={{ childrenGap: 30}}
+                >
+                    {
+                        currentGame.players.map(x =>(
+                            <DefaultButton
+                                styles={{root: {paddingTop: 20, paddingBottom: 20, marginTop: 40}}}
+                            >
+                                <Text
+                                    variant='large'
+                                >
+                                    {x.name}
+                                </Text>
+                            </DefaultButton>
+                        ))
+                    }
+                </Stack>
+            </Panel>
             <Text variant='large'>
                 Started: {currentGame.start}
             </Text>
